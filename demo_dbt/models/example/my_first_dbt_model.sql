3c0963ba59a451b69a7272788d93c2f89aee6a2a
MD Aminul Haque <md.aminul.haque.7@gmail.com>
2021-03-05 02:29:10 +0100

    testing the first push. dated 05.03.2021

--- demo_dbt/models/example/my_first_dbt_model.sql
+++ demo_dbt/models/example/my_first_dbt_model.sql
@@ -9,6 +9,7 @@
 
 {{ config(materialized='table') }}
 
+/*
 with source_data as (
 
     select 1 as id
@@ -17,8 +18,33 @@ with source_data as (
 
 )
 
-select *
-from source_data
+
+with source_data2 as (
+
+    select * from report.data
+
+)
+
+*/
+
+with source_beers as (
+
+    select id as beer_id,upper(name) as beer_name, upper(tagline) as beer_tagline, upper(first_brewed)  as beer_first_brewed, abv as beer_abv   from report.stg_beers
+
+),
+
+ source_beers_food_pairing as (
+
+    select *  from report.stg_beers_food_pairing
+
+),
+
+joined_beer as (
+select * from
+ source_beers, source_beers_food_pairing where source_beers.beer_id= source_beers_food_pairing.id
+)
+
+select * from joined_beer
 
 /*
     Uncomment the line below to remove records with null `id` values
--- demo_dbt/models/example/my_third_dbt_model.sql
+++ demo_dbt/models/example/my_third_dbt_model.sql
@@ -1,6 +1,6 @@
 
 -- Use the `ref` function to select from other models
 
-select *--id, id::int4 +7 as plus_7
+select count(*) as count_of_beers--id, id::int4 +7 as plus_7
 from {{ ref('my_first_dbt_model') }}
 --where id = 1
